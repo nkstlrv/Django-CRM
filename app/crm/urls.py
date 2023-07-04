c807cd4222d4ff7d5c810e10f11875fa2e1cf790
nkstlrv <stlrvnk@gmail.com>
2023-07-04 18:52:39 -0400

    User registration

--- app/crm/urls.py
+++ app/crm/urls.py
@@ -4,4 +4,5 @@ from crm import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
 ]
--- app/crm/views.py
+++ app/crm/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from crm.forms import RegisterForm
 
 
 def index(request):
     if request.method == 'POST':
         username = request.POST['username']
-        password = request.POST['password']
-        
+        password = request.POST.get('password')
+
         user = authenticate(request, username=username, password=password)
-        
+
         if user:
             login(request, user)
             messages.success(request, "You have logged in")
             return redirect('home')
         else:
             messages.success(request, "Wrong credentials. Please try again")
-            
 
     return render(request, 'crm/index.html')
 
@@ -26,3 +26,23 @@ def logout_view(request):
     messages.success(request, "You have been logged out")
     return redirect('home')
 
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have been successfully registered")
+            
+            return redirect('home')
+    else:
+        form = RegisterForm()
+        return render(request, 'crm/register.html', {'form': form})
+    return render(request, 'crm/register.html', {'form': form})
