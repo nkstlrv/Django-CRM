bcb782ec4c520c85c044f4b659576165a2fd6c92
nkstlrv <stlrvnk@gmail.com>
2023-07-04 17:08:20 -0400

    login form

--- app/crm/urls.py
+++ app/crm/urls.py
@@ -3,4 +3,5 @@ from crm import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('logout/', views.logout, name='logout'),
 ]
--- app/crm/views.py
+++ app/crm/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def index(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user:
+            login(request, user)
+            messages.success(request, "You have logged in")
+            return redirect('home')
+        else:
+            messages.success(request, "Wrong credentials. Please try again")
+            
+
     return render(request, 'crm/index.html')
-    
+
+
+def logout(request):
+    pass
